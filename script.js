9b6b38964fd4d3d190ccf0c8602bd8c13409661c
DhruvSingh7077 <dhruvsingh7077@gmail.com>
2025-04-26 21:57:23 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,56 @@
- document.onkeydown = function (e) {
-    console.log("key code is: ", e.key);
-    if(e.key === "ArrowUp "){
+ score = 0 ;
+ cross =true;
+ document.onkeydown = function(e){
+    console.log("key code is: ", e.keyCode);
+    if(e.keyCode==38){
         dino = document.querySelector('.dino');
         dino.classList.add('animateDino');
         setTimeout(()=>{
             
-            dino.classList.remove('animateDino');
+            dino.classList.remove('animateDino')
         },700);
     }
- }
+ 
+    if(e.keyCode==39){
+        dino = document.querySelector('.dino');
+        dinoX = parseInt(window.getComputedStyle(dino, null).getPropertyValue('left'));
+        dino.style.left = dinoX + 112 + "px";
+       
+    }
+    if(e.keyCode==37){
+        dino = document.querySelector('.dino');
+        dinoX = parseInt(window.getComputedStyle(dino, null).getPropertyValue('left'));
+        dino.style.left = (dinoX - 112) + "px";
+       
+    }
+}
+ 
  setInterval(()=>{
     dino = document.querySelector('.dino');
     gameOver = document.querySelector('.gameOver');
     obstacle = document.querySelector('.obstacle');
     
- },100);
+    dx = parseInt(window.getComputedStyle(dino, null).getPropertyValue('left'));
+    dy = parseInt(window.getComputedStyle(dino, null).getPropertyValue('right'));
+    ox = parseInt(window.getComputedStyle(obstacle, null).getPropertyValue('left'));
+    oy = parseInt(window.getComputedStyle(obstacle, null).getPropertyValue('right'));
+
+    offsetX = math.abs(dx - ox);
+    offsetY = math.abs(dy - oy);
+    if(offsetX<93  && offsetY<53){
+        gameOver.style.visibility = 'visible';
+        obstacle.classList.remove('obstacleAni') 
+    }
+    else if(offsetX,145 && cross){
+        score+=1;
+        updateScore(score);
+        cross = false;
+        set
+        timeo
+   }
+
+},100);
+
+function updateScore(score){
+    scoreCont.innerHTML = "your Score: " + score
+}
